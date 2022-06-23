39086c7fd0eafb61df37d224863db31a3edce4de
MCK-cachan <105218015+MCK-cachan@users.noreply.github.com>
2022-06-23 14:57:26 +0200

    Create SampleFilter.h

---
+++ SampleFilter.h
@@ -0,0 +1,18 @@
+#ifndef SAMPLEFILTER_H_
+#define SAMPLEFILTER_H_
+#define ORDRE 2
+
+
+/*
+SOS Matrix:                                                                                       
+1  -2  1  1  -1.965105316133364699027197275427170097828  0.965703783490617473361794509401079267263
+                                                                                                  
+Scale Values:                                                                                     
+0.982702274905995598608399177464889362454                                                         
+
+
+*/
+double IIRFloat(double input);
+
+
+#endif
